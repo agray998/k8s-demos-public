5a6fb4a8dd80b7adb84741546990d8f301420d00
agray <agray@qa.com>
2025-05-14 12:16:48 +0100

    add resources to volumes pod, update rook script to install ALL storageclasses

--- volumes/install-rook.sh
+++ volumes/install-rook.sh
@@ -4,9 +4,11 @@ cd rook/deploy/examples
 kubectl apply -f crds.yaml -f common.yaml -f operator.yaml 
 kubectl apply -f cluster-test.yaml 
 kubectl apply -f csi/rbd/storageclass-test.yaml 
-kubectl apply -f filesystem-test.yaml 
+kubectl apply -f filesystem-test.yaml
+kubectl apply -f csi/cephfs/storageclass.yaml
 kubectl apply -f object-test.yaml 
 kubectl apply -f object-user.yaml 
+kubectl apply -f storageclass-bucket-delete.yaml
 kubectl get all -n rook-ceph
 kubectl -n rook-ceph get cephcluster rook-ceph
 kubectl -n rook-ceph get cephclusters
