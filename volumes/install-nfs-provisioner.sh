1e06a3fe135bc0b425a0426184f8817cc4f40a24
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-05-07 12:21:48 +0100

    Update install-nfs-provisioner.sh

--- volumes/install-nfs-provisioner.sh
+++ volumes/install-nfs-provisioner.sh
@@ -3,4 +3,4 @@ helm repo add nfs-provisioner \
 	https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm repo update
 helm install nfs-provisioner nfs-provisioner/nfs-subdir-external-provisioner \
-	--set nfs.server=k8scp,nfs.path=/opt/sfw,storageClass.volumeBindingMode=WaitForFirstConsumer
+	--set nfs.server=cp,nfs.path=/opt/sfw,storageClass.volumeBindingMode=WaitForFirstConsumer
