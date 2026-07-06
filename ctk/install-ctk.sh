bac7064ff77b9f55a9b9a36bbc528fc7df426c69
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-07-06 13:11:15 +0100

    Update install-ctk.sh

--- ctk/install-ctk.sh
+++ ctk/install-ctk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/chaostoolkit-incubator/kubernetes-crd.git ctk-operator
 cd ctk-operator/manifests
-kubectl kustomize manifests/overlays/generic-rbac | kubectl apply -f -
+kubectl kustomize overlays/generic-rbac | kubectl apply -f -
